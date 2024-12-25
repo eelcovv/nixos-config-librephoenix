a2a1841f26003c10a15f6a9938275e1b7dba14c2
Eelco van Vliet <>
2024-12-25 03:02:47 +0100

    turned shell integration off

--- user/app/terminal/kitty.nix
+++ user/app/terminal/kitty.nix
@@ -6,8 +6,8 @@
   ];
   programs.kitty.enable = true;
   programs.kitty.settings = {
-    shell_integration = "disabled";
     background_opacity = lib.mkForce "0.85";
     modify_font = "cell_width 90%";
+    shell_integration = "disabled";
   };
 }
