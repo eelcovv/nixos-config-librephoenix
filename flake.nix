d51fc65176edeac482710e9d2e7f60f7c0d2d562
Eelco van Vliet <>
2024-12-25 20:43:27 +0100

    added email

--- flake.nix
+++ flake.nix
@@ -21,13 +21,14 @@
       userSettings = rec {
         username = "eelco"; # username
         name = "Eelco van Vliet"; # name/identifier
-        email = ""; # email (used for certain configurations)
+        email = "eelcovv@gmail.com"; # email (used for certain configurations)
         dotfilesDir = "/home/eelco/.dotfiles"; # absolute path of the local repo
         theme = "ashes"; # selcted theme from my themes directory (./themes/)
-	# Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
-        wm = "hyprland"; 
-        # wm = "plasma"; 
-		# window manager type (hyprland or x11) translator
+        # Selected window manager or desktop environment; 
+        # must select one in both ./user/wm/ and ./system/wm/
+        #wm = "hyprland"; 
+        wm = "plasma"; 
+        # window manager type (hyprland or x11) translator
         wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
         browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
         spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
