205f0eb4fc95bb31af86e1821c6ca54dee7ef8d4
Eelco van Vliet <>
2024-12-25 02:11:29 +0100

    changed theme

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         name = "Eelco van Vliet"; # name/identifier
         email = ""; # email (used for certain configurations)
         dotfilesDir = "/home/eelco/.dotfiles"; # absolute path of the local repo
-        theme = "io"; # selcted theme from my themes directory (./themes/)
+        theme = "isotope"; # selcted theme from my themes directory (./themes/)
         wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
         # window manager type (hyprland or x11) translator
         wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
