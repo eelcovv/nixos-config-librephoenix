209580599673fa6a384e35a01ca5e6d210a25bb4
Eelco <eelcovv@gmail.com>
2025-01-03 18:46:23 +1000

    reformat and changed to homelab for testing

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       systemSettings = {
         system = "x86_64-linux"; # system arch
         hostname = "snowball"; # hostname
-        profile = "personal"; # select a profile defined from my profiles directory
+        profile = "homelab"; # select a profile defined from my profiles directory
         timezone = "Europe/Amsterdam"; # select timezone
         locale = "en_US.UTF-8"; # select locale
         bootMode = "uefi"; # uefi or bios
@@ -15,7 +15,7 @@
         grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
         # gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
         gpuType = "nvidia"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
-        stable_nix_profiles = ["homelab"  "worklab"  "personal"  "work"];
+        stable_nix_profiles = [ "homelab" "worklab" "personal" "work" ];
       };
 
 
@@ -28,7 +28,7 @@
         theme = "ashes"; # selcted theme from my themes directory (./themes/)
         # Selected window manager or desktop environment; 
         # must select one in both ./user/wm/ and ./system/wm/
-        wm = "hyprland"; 
+        wm = "hyprland";
         # wm = "plasma"; 
         # window manager type (hyprland or x11) translator
         wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
@@ -44,18 +44,19 @@
         # generates a command that can be used to spawn editor inside a gui
         # EDITOR and TERM session variables must be set in home.nix or other module
         # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
-        spawnEditor = if (editor == "emacsclient") then
-                        "emacsclient -c -a 'emacs'"
-                      else
-                        (if ((editor == "vim") ||
-                             (editor == "nvim") ||
-                             (editor == "nano")) then
-                               "exec " + term + " -e " + editor
-                         else
-                         (if (editor == "neovide") then
-                           "neovide -- --listen /tmp/nvimsocket" 
-                           else
-                           editor));
+        spawnEditor =
+          if (editor == "emacsclient") then
+            "emacsclient -c -a 'emacs'"
+          else
+            (if ((editor == "vim") ||
+              (editor == "nvim") ||
+              (editor == "nano")) then
+              "exec " + term + " -e " + editor
+            else
+              (if (editor == "neovide") then
+                "neovide -- --listen /tmp/nvimsocket"
+              else
+                editor));
       };
 
       lib-stable = inputs.nixpkgs-stable.lib;
@@ -68,9 +69,10 @@
         (import inputs.nixpkgs { system = systemSettings.system; rocmSupport = (if systemSettings.gpu == "amd" then true else false); }).applyPatches {
           name = "nixpkgs-patched";
           src = inputs.nixpkgs;
-          patches = [ #./patches/emacs-no-version-check.patch
-                      #./patches/nixpkgs-348697.patch
-                    ];
+          patches = [
+            #./patches/emacs-no-version-check.patch
+            #./patches/nixpkgs-348697.patch
+          ];
         };
 
       pkgs-stable = import inputs.nixpkgs-stable {
@@ -94,11 +96,11 @@
       # use nixpkgs if running a server (homelab or worklab profile)
       # otherwise use patched nixos-unstable nixpkgs
       pkgs = (if (use_nix_stable)
-              then
-                pkgs-stable
-              else
-                pkgs-unstable
-             );
+      then
+        pkgs-stable
+      else
+        pkgs-unstable
+      );
 
       pkgs-emacs = import inputs.emacs-pin-nixpkgs {
         system = systemSettings.system;
@@ -116,19 +118,19 @@
       # use nixpkgs if running a server (homelab or worklab profile)
       # otherwise use patched nixos-unstable nixpkgs
       lib = (if (use_nix_stable)
-             then
-               lib-stable
-             else
-               lib-unstable
-            );
+      then
+        lib-stable
+      else
+        lib-unstable
+      );
 
       # use home-manager-stable if running a server (homelab or worklab profile)
       # otherwise use home-manager-unstable
       home-manager = (if (use_nix_stable)
-             then
-               inputs.home-manager-stable
-             else
-               inputs.home-manager-unstable);
+      then
+        inputs.home-manager-stable
+      else
+        inputs.home-manager-unstable);
 
       # Systems that can run tests:
       supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
@@ -140,7 +142,8 @@
       nixpkgsFor =
         forAllSystems (system: import inputs.nixpkgs { inherit system; });
 
-    in {
+    in
+    {
       homeConfigurations = {
         user = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
