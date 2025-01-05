d82d10c065224c6fa143223e1985903b24e7bcb9
Eelco <eelcovv@gmail.com>
2025-01-05 12:00:57 +0100

    turned on home

--- profiles/base/home.nix
+++ profiles/base/home.nix
@@ -9,25 +9,25 @@
   programs.home-manager.enable = true;
 
   imports = [
-            #   (./. + "../../../user/wm"+("/"+userSettings.wm+"/"+userSettings.wm)+".nix") # My window manager selected from flake
-            #   ../../user/shell/sh.nix # My zsh and bash config
-            #   ../../user/shell/cli-collection.nix # Useful CLI apps
-            #   #../../user/app/doom-emacs/doom.nix # My doom emacs config
-            #   ../../user/app/nvim/nvim.nix # My doom emacs config
-            #   #../../user/app/emacsng # Me experimenting with emacsng and a vanilla config
-            #   ../../user/app/ranger/ranger.nix # My ranger file manager config
-            #   ../../user/app/git/git.nix # My git config
-            #   # ../../user/app/texlive/texlive.nix # My texlive config
-            #   ../../user/app/keepass/keepass.nix # My password manager
-            #   (./. + "../../../user/app/browser"+("/"+userSettings.browser)+".nix") # My default browser selected from flake
-            #   ../../user/app/virtualization/virtualization.nix # Virtual machines
-            #   #../../user/app/flatpak/flatpak.nix # Flatpaks
-            #   ../../user/style/stylix.nix # Styling and themes for my apps
-            #   ../../user/lang/cc/cc.nix # C and C++ tools
-            #   ../../user/lang/godot/godot.nix # Game development
-            #   #../../user/pkgs/blockbench.nix # Blockbench ## marked as insecure
-            #   ../../user/hardware/bluetooth.nix # Bluetooth
-            # ];
+              (./. + "../../../user/wm"+("/"+userSettings.wm+"/"+userSettings.wm)+".nix") # My window manager selected from flake
+              ../../user/shell/sh.nix # My zsh and bash config
+              ../../user/shell/cli-collection.nix # Useful CLI apps
+              #../../user/app/doom-emacs/doom.nix # My doom emacs config
+              ../../user/app/nvim/nvim.nix # My doom emacs config
+              #../../user/app/emacsng # Me experimenting with emacsng and a vanilla config
+              ../../user/app/ranger/ranger.nix # My ranger file manager config
+              ../../user/app/git/git.nix # My git config
+              # ../../user/app/texlive/texlive.nix # My texlive config
+              ../../user/app/keepass/keepass.nix # My password manager
+              (./. + "../../../user/app/browser"+("/"+userSettings.browser)+".nix") # My default browser selected from flake
+              ../../user/app/virtualization/virtualization.nix # Virtual machines
+              #../../user/app/flatpak/flatpak.nix # Flatpaks
+              ../../user/style/stylix.nix # Styling and themes for my apps
+              ../../user/lang/cc/cc.nix # C and C++ tools
+              ../../user/lang/godot/godot.nix # Game development
+              #../../user/pkgs/blockbench.nix # Blockbench ## marked as insecure
+              ../../user/hardware/bluetooth.nix # Bluetooth
+            ];
 
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
@@ -207,54 +207,54 @@
   #     extension = "svg";
   #   };
 
-  # services.syncthing.enable = true;
-  # services.nextcloud-client = {
-  #   enable = false;
-  #   startInBackground = true;
-  # };
+  services.syncthing.enable = true;
+  services.nextcloud-client = {
+    enable = false;
+    startInBackground = true;
+  };
 
-  # xdg.enable = true;
-  # xdg.userDirs = {
-  #   enable = true;
-  #   createDirectories = true;
-  #   music = "${config.home.homeDirectory}/Media/Music";
-  #   videos = "${config.home.homeDirectory}/Media/Videos";
-  #   pictures = "${config.home.homeDirectory}/Media/Pictures";
-  #   templates = "${config.home.homeDirectory}/Templates";
-  #   download = "${config.home.homeDirectory}/Downloads";
-  #   documents = "${config.home.homeDirectory}/Documents";
-  #   desktop = null;
-  #   publicShare = null;
-  #   extraConfig = {
-  #     XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
-  #     XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
-  #     XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
-  #     XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
-  #     XDG_PODCAST_DIR = "${config.home.homeDirectory}/Media/Podcasts";
-  #     XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
-  #   };
-  # };
-  # xdg.mime.enable = true;
-  # xdg.mimeApps.enable = true;
-  # xdg.mimeApps.associations.added = {
-  #   # TODO fix mime associations, most of them are totally broken :(
-  #   "application/octet-stream" = "flstudio.desktop;";
-  # };
+  xdg.enable = true;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    music = "${config.home.homeDirectory}/Media/Music";
+    videos = "${config.home.homeDirectory}/Media/Videos";
+    pictures = "${config.home.homeDirectory}/Media/Pictures";
+    templates = "${config.home.homeDirectory}/Templates";
+    download = "${config.home.homeDirectory}/Downloads";
+    documents = "${config.home.homeDirectory}/Documents";
+    desktop = null;
+    publicShare = null;
+    extraConfig = {
+      XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
+      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
+      XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
+      XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
+      XDG_PODCAST_DIR = "${config.home.homeDirectory}/Media/Podcasts";
+      XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
+    };
+  };
+  xdg.mime.enable = true;
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.associations.added = {
+    # TODO fix mime associations, most of them are totally broken :(
+    "application/octet-stream" = "flstudio.desktop;";
+  };
 
-  # home.sessionVariables = {
-  #   EDITOR = userSettings.editor;
-  #   SPAWNEDITOR = userSettings.spawnEditor;
-  #   TERM = userSettings.term;
-  #   BROWSER = userSettings.browser;
-  # };
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    SPAWNEDITOR = userSettings.spawnEditor;
+    TERM = userSettings.term;
+    BROWSER = userSettings.browser;
+  };
 
-  # news.display = "silent";
+  news.display = "silent";
 
-  # gtk.iconTheme = {
-  #   package = pkgs.papirus-icon-theme;
-  #   name = if (config.stylix.polarity == "dark") then "Papirus-Dark" else "Papirus-Light";
-  # };
+  gtk.iconTheme = {
+    package = pkgs.papirus-icon-theme;
+    name = if (config.stylix.polarity == "dark") then "Papirus-Dark" else "Papirus-Light";
+  };
 
-  # services.pasystray.enable = true;
+  services.pasystray.enable = true;
 
 }
